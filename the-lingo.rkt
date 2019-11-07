27d01168ce9a44af042490ef62bc2e1b2257a6b6
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2019-11-07 20:08:41 +0800

    更

--- the-lingo.rkt
+++ the-lingo.rkt
@@ -34,7 +34,8 @@
 {define:type hash-t hash/c}
 {define:type arrayof-t listof} ;; A array is a variable length vector
 {define (create-array . xs) xs}
-{define (linear-array-add-element xs x) (cons x xs)} ;; Add a element to a array.Can change source array."linear-" means linear type system
+{define (linear-array-add-element xs x) (cons x xs)} ;; Add a element to a array.Can destory source array."linear-" means linear type system
+{define (linear-array-append xs ys) (append xs ys)}
 {define-syntax-rule {array-foreach xs v . c} {for ([v xs]) . c}}
 
 {define:type nothing-t void-t}
@@ -51,6 +52,7 @@
    (rec-type value-char-t)
    (rec-type value-just-t)
    (rec-type value-delay-t)
+   (rec-type value-comment-t)
    )}
 {define:type env-t (hash-t value-t value-t)}
 
@@ -78,7 +80,7 @@
 {define:type value-char-t (and-t value-struct-t (vector-t value-char-t-id-t char-t nothing-t nothing-t))}
 {define:type value-just-t (and-t value-struct-t (vector-t value-just-t-id-t value-t nothing-t nothing-t))}
 {define:type value-delay-t (and-t value-struct-t (vector-t value-delay-t-id-t (-> value-t) (-> (vector-t (rec-type env-t) value-t)) nothing-t))} ;; exec:`(-> value-t)`/display:`(-> (vector-t env-t value-t))`
-{define:type value-comment-t (and-t value-struct-t (vector-t value-comment-t-id-t value-t value-t nothing-t))} ;; val:value-t/comment:value-t
+{define:type value-comment-t (and-t value-struct-t (vector-t value-comment-t-id-t value-t (arrayof-t value-t) nothing-t))} ;; val:value-t/comment:`(arrayof-t value-t)`
 
 {define/t (value-symbol? x)
   (-> value-t boolean-t)
@@ -131,10 +133,10 @@
   (-> value-char-t char-t)
   (vector-ref x 1)}
 {define/t (cons-value-comment x comment)
-  (-> value-t value-t value-comment-t)
+  (-> value-t (arrayof-t value-t) value-comment-t)
   (vector value-comment-t-id x comment nothing)}
 {define/t (elim-value-comment x)
-  (-> value-comment-t (vector-t value-t value-t))
+  (-> value-comment-t (vector-t value-t (arrayof-t value-t)))
   (vector (vector-ref x 1) (vector-ref x 2))}
 
 {define/t (value-unsafe-set-to-just! x v)
@@ -147,15 +149,18 @@
 {define/t (must-value-unjust-1 x)
   (-> value-just-t value-t)
   (vector-ref x 1)}
-{define/t (value-unjust x)
+{define/t (value-unjust-* x)
   (-> value-t value-t)
-  (value-unjust-aux x (create-array x))}
-{define/t (value-unjust-aux x history)
+  (value-unjust-*-aux x (create-array x))}
+{define/t (value-unjust-*-aux x history)
   (-> value-t (arrayof-t value-t) value-t)
-  (if (value-just? x) (value-unjust-aux (must-value-unjust-1 x) (linear-array-add-element history x))
+  (if (value-just? x) (value-unjust-*-aux (must-value-unjust-1 x) (linear-array-add-element history x))
       {begin
         {array-foreach history history_v (value-unsafe-set-to-just! history_v x)}
         x})}
+{define/t (cons-value-delay exec display_f)
+  (-> (-> value-t) (-> (vector-t env-t value-t)) value-delay-t)
+  (vector value-delay-t-id exec display_f nothing)}
 {define/t (must-nocache-value-force-1 x)
   (-> value-delay-t value-t)
   {let/t ([exec (-> value-t) (vector-ref x 1)])
@@ -172,3 +177,16 @@
     [else
      {array-foreach history history_v (value-unsafe-set-to-just! history_v x)}
      x]}}
+{define/t (value-undelay-1_>>= x display_f f)
+  (-> value-t (-> (vector-t env-t value-t)) (-> value-t (arrayof-t value-t) any-t) any-t)
+  (value-undelay-1_>>=_aux x display_f (create-array) f)}
+{define/t (value-undelay-1_>>=_aux x display_f comments f)
+  (-> value-t (-> (vector-t env-t value-t)) (arrayof-t value-t) (-> value-t (arrayof-t value-t) any-t) any-t)
+  {cond
+    [(value-comment? x)
+     {let/t ([r (vector-t value-t (arrayof-t value-t)) (elim-value-comment x)])
+       {let/t ([new-v value-t (vector-ref r 0)] [new-comments (arrayof-t value-t) (vector-ref r 1)])
+              (value-undelay-1_>>=_aux new-v (linear-array-append comments new-comments) f)}}]
+    [(value-just? x) (value-undelay-1_>>=_aux (value-unjust-* x) display_f comments f)]
+    [(value-delay? x) "WIP"]
+    [else "WIP"]}}
