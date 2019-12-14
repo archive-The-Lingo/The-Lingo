c757683cf6e30b610503caf64475ef27863eab17
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2019-12-14 18:59:50 +0800

    删comment系統

--- the-lingo.rkt
+++ the-lingo.rkt
@@ -111,8 +111,6 @@
 {define/t value-just-t-id value-just-t-id-t 5}
 {define:type value-delay-t-id-t (and-tt t-id-t 6)}
 {define/t value-delay-t-id value-delay-t-id-t 6}
-{define:type value-comment-t-id-t (and-tt t-id-t 7)}
-{define/t value-comment-t-id value-comment-t-id-t 7}
 
 {define-syntax-rule:type (value-tt x) (t->? (and-tt value-bone-t x))} ;; without t->?, it will make the same value different and disallow changing the type of value
 {define:type value-symbol-t (value-tt (vector-tt value-symbol-t-id-t string-t nothing-t nothing-t))}
@@ -122,7 +120,6 @@
 {define:type value-char-t (value-tt (vector-tt value-char-t-id-t char-t nothing-t nothing-t))}
 {define:type value-just-t (value-tt (vector-tt value-just-t-id-t value-t nothing-t nothing-t))}
 {define:type value-delay-t (value-tt (vector-tt value-delay-t-id-t (-> value-t) (-> (vector-tt identifierspace-t value-t)) nothing-t))} ;; exec:`(-> value-t)`/display:`(-> (vector-t identifierspace-t value-t))`
-{define:type value-comment-t (value-tt (vector-tt value-comment-t-id-t value-t (list-of-tt value-t) nothing-t))} ;; val:value-t/comment:`(arrayof-t value-t)`
 {define:type value-t
   (t->?
    (or-tt
@@ -133,7 +130,6 @@
     value-char-t
     value-just-t
     value-delay-t
-    value-comment-t
     ))}
 
 {define/t (value-symbol? x)
@@ -157,9 +153,6 @@
 {define/t (value-delay? x)
   (-> value-t boolean-t)
   (= (vector-ref x 0) value-delay-t-id)}
-{define/t (value-comment? x)
-  (-> value-t boolean-t)
-  (= (vector-ref x 0) value-comment-t-id)}
 
 {define/t (cons-value-symbol x)
   (-> string-t value-symbol-t)
@@ -189,34 +182,10 @@
 {define/t (elim-value-char x)
   (-> value-char-t char-t)
   (vector-ref x 1)}
-{define/t (must-cons-value-comment x comment)
-  (-> value-t (and-tt (not-tt null-t) (list-of-tt value-t)) value-comment-t)
-  (vector value-comment-t-id x comment nothing)}
-{define/t (cons-value-comment x comment)
-  (-> value-t (list-of-tt value-t) value-t)
-  (if (null? comment) x (must-cons-value-comment x comment))}
-{define/t (elim-value-comment x)
-  (-> value-comment-t (vector-tt value-t (list-of-tt value-t)))
-  (vector (vector-ref x 1) (vector-ref x 2))}
 {define/t (cons-value-delay exec display_f)
   (-> (-> value-t) (-> (vector-tt identifierspace-t value-t)) value-delay-t)
   (vector value-delay-t-id exec display_f nothing)}
 
-{define/t (elim-value-comment-* x)
-  (-> value-comment-t (vector-tt value-t (list-of-tt value-t)))
-  (elim-value-comment-*-aux x (list x) (list))}
-{define/t (elim-value-comment-*-aux x history comments)
-  (-> value-t (list-of-tt value-t) (list-of-tt value-t) (vector-tt value-t (list-of-tt value-t)))
-  {cond
-    [(value-just? x) (elim-value-comment-*-aux (value-unjust-* x) (cons history x) comments)]
-    [(value-comment? x) {let ([x01 (elim-value-comment x)])
-                          (elim-value-comment-*-aux
-                           (vector-ref x 0)
-                           (cons history x)
-                           (append comments (vector-ref x 1)))}]
-    [else
-     {list-foreach history history_v (value-unsafe-set-to-just! history_v x)}
-     (vector x comments)]}}
 {define/t (value-unsafe-set-to-just! x v)
   (-> value-t value-t void-t)
   {when (not (eq? x v))
@@ -266,34 +235,22 @@
   (-> (cont-tt any-t any-t) (-> any-t (cont-tt any-t any-t)) (cont-tt any-t any-t))
   {λ (c) (x {λ (v) ((f v) c)})}}
 {define/t ((value-undelay-m x display_f) f)
-  (-> value-t (-> (vector-tt identifierspace-t value-t)) (cont-tt (vector-tt value-t (list-of-tt value-t)) value-t))
+  (-> value-t (-> (vector-tt identifierspace-t value-t)) (cont-tt value-t value-t))
   (value-undelay-m-aux x display_f (list) f)}
-{define/t (value-undelay-m-aux x display_f comments f)
-  (-> value-t (-> (vector-tt identifierspace-t value-t)) (list-of-tt value-t) (-> (vector-tt value-t (list-of-tt value-t)) value-t) value-t)
+{define/t (value-undelay-m-aux x display_f f)
+  (-> value-t (-> (vector-tt identifierspace-t value-t)) (-> value-t value-t) value-t)
   {cond
-    [(value-comment? x)
-     {let/t ([x01 (vector-tt value-t (list-of-tt value-t)) (elim-value-comment-* x)])
-            {let/t ([new-v value-t (vector-ref x01 0)] [new-comments (list-of-tt value-t) (vector-ref x01 1)])
-                   (value-undelay-m-aux new-v (append comments new-comments) f)}}]
-    [(value-just? x) (value-undelay-m-aux (value-unjust-* x) display_f comments f)]
-    [(value-delay? x) (cons-value-delay {λ () (value-undelay-m-aux (must-value-force-1 x) display_f comments f)} display_f)]
-    [else (f (vector x comments))]}}
+    [(value-just? x) (value-undelay-m-aux (value-unjust-* x) display_f f)]
+    [(value-delay? x) (cons-value-delay {λ () (value-undelay-m-aux (must-value-force-1 x) display_f f)} display_f)]
+    [else (f x)]}}
 
 {define/t (value-force+equal? x y)
   (-> value-t value-t boolean-t)
-  (value-force+equal?-aux x y (box #f))}
-{define/t (value-force+equal?-aux x y inner-equal-and-has-comment)
-  (-> value-t value-t (box-t boolean-t) boolean-t)
   (if (eq? x y)
       #t
       {let/t ([x value-t (value-force x)] [y value-t (value-force y)])
              {cond
                [(eq? x y) #t]
-               [(value-comment? x)
-                {let/t ([x01 (vector-tt value-t (list-of-tt value-t)) (elim-value-comment-* x)])
-                       (set-box! inner-equal-and-has-comment #t)
-                       (value-force+equal?-aux (vector-ref x 0) y inner-equal-and-has-comment)}]
-               [(value-comment? y) (value-force+equal?-aux y x inner-equal-and-has-comment)]
                [(value-null? x) {if (value-null? y)
                                     {begin
                                       (value-unsafe-set-to-just! x y)
@@ -311,28 +268,14 @@
                                     #f}]
                [(value-pair? x) {if (value-pair? y)
                                     {let ([x01 (elim-value-pair x)] [y01 (elim-value-pair y)])
-                                      (value-force+equal?-aux-aux-pair
-                                       x (vector-ref x01 0) (vector-ref x01 1)
-                                       y (vector-ref y01 0) (vector-ref y01 1)
-                                       inner-equal-and-has-comment)}
+                                      (and (value-force+equal? (vector-ref x01 0) (vector-ref y01 0))
+                                           (value-force+equal? (vector-ref x01 1) (vector-ref y01 1)))}
                                     #f}]
                [(value-struct? x) {if (value-struct? y)
                                     {let ([x01 (elim-value-struct x)] [y01 (elim-value-struct y)])
-                                      (value-force+equal?-aux-aux-pair
-                                       x (vector-ref x01 0) (vector-ref x01 1)
-                                       y (vector-ref y01 0) (vector-ref y01 1)
-                                       inner-equal-and-has-comment)}
+                                      (and (value-force+equal? (vector-ref x01 0) (vector-ref y01 0))
+                                           (value-force+equal? (vector-ref x01 1) (vector-ref y01 1)))}
                                     #f}]}})}
-{define/t (value-force+equal?-aux-aux-pair x x0 x1 y y0 y1 inner-has-comment)
-  (-> value-t value-t value-t value-t value-t value-t (box-t boolean-t) boolean-t)
-  {let/t ([xy-equal-and-has-comment (box-t boolean-t) (box #f)])
-         {if (and (value-force+equal?-aux x0 y0 xy-equal-and-has-comment) (value-force+equal?-aux x1 y1 xy-equal-and-has-comment))
-             {begin
-               {if (unbox xy-equal-and-has-comment)
-                   (set-box! inner-has-comment #t)
-                   (value-unsafe-set-to-just! x y)}
-               #t}
-             #f}}}
 
 {define/t exp-id-s value-symbol-t (cons-value-symbol "標符")}
 
@@ -344,14 +287,12 @@
   (-> identifierspace-t value-t
       (cont-tt value-t value-t))
   {do cont->>=
-    {<- x-comments (value-undelay-m x {λ () (vector space x)})}
-    {:= x (vector-ref x-comments 0)}
-    {:= comments (vector-ref x-comments 1)}
+    {<- x (value-undelay-m x {λ () (vector space x)})}
     {if (value-struct? x)
         {do cont->>=
           {:= ast (elim-value-struct x)}
-          {<- ast-type--comments (value-undelay-m (vector-ref ast 0) {λ () (vector space x)})}
-          {<- ast-list--comments (value-undelay-m (vector-ref ast 1) {λ () (vector space x)})}
+          {<- ast-type (value-undelay-m (vector-ref ast 0) {λ () (vector space x)})}
+          {<- ast-list (value-undelay-m (vector-ref ast 1) {λ () (vector space x)})}
           {cond
             [else (WIP)]}
           }
