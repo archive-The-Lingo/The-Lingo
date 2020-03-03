a6fd1d1eed49b1124520c9435edb6f6cd657befc
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-03-03 16:52:50 +0800

    更

--- rust/the-lingo/src/lib.rs
+++ rust/the-lingo/src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::{sync::{Arc, Mutex}, fmt};
+use futures::prelude::Future;
+
+#[derive(Debug)]
+pub struct Value (Arc<Mutex<ValueUnpacked>>);
+
+#[derive(Debug)]
+enum ValueUnpacked {
+    Null,
+    Symbol(String),
+    Pair(Value,Value),
+    Struct(Value,Value),
+    Just(Value),
+    Delay(Box<ValueUnpackedDelay>)
+}
+
+struct ValueUnpackedDelay {
+    countinue: dyn Future<Output = Value> + Send + Sync
+}
+impl fmt::Debug for ValueUnpackedDelay {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "ValueUnpackedDelay {{ }}")
     }
 }
