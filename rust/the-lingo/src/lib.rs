7a566bc9f553e1f725f0fdefe5b9e0ea4b4cbf06
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-03-04 13:46:55 +0800

    更

--- rust/the-lingo/src/lib.rs
+++ rust/the-lingo/src/lib.rs
@@ -1,8 +1,19 @@
 use std::{sync::{Arc, Mutex}, fmt};
 use futures::prelude::Future;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Value (Arc<Mutex<ValueUnpacked>>);
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        Arc::ptr_eq(&self.0, &other.0)
+    }
+}
+impl Eq for Value {}
+impl Value {
+    async fn forced_equal(&self, other: &Self) -> bool {
+        panic!("TODO")
+    }
+}
 
 #[derive(Debug)]
 enum ValueUnpacked {
@@ -11,14 +22,39 @@ enum ValueUnpacked {
     Pair(Value,Value),
     Struct(Value,Value),
     Just(Value),
-    Delay(Box<ValueUnpackedDelay>)
+    Delay(ValueUnpackedDelay),
+    Optimized(OptimizedValue)
 }
-
 struct ValueUnpackedDelay {
-    countinue: dyn Future<Output = Value> + Send + Sync
+    countinue: Box<dyn Future<Output = Value> + Send + Sync>,
+    stop: Box<dyn Future<Output = Value> + Send + Sync>
 }
 impl fmt::Debug for ValueUnpackedDelay {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "ValueUnpackedDelay {{ }}")
     }
 }
+
+#[derive(Debug)]
+enum OptimizedValue {
+    Mapping(Mapping)
+}
+
+#[derive(Debug)]
+struct Mapping (Box<Vec<(Value, Value)>>);
+impl Mapping {
+    pub async fn set(&self, k: Value, v: Value) -> Self {
+        let mut result = self.0.clone();
+        result.push((k, v));
+        Mapping(result)
+    }
+    pub async fn get(&self, to_find: &Value) -> Option<&Value> {
+        for (k, v) in self.0.iter() {
+            if to_find.forced_equal(k).await {
+                return Option::Some(v);
+            }
+        }
+        Option::None
+    }
+}
+
