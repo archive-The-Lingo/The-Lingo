7b92726b2c77cd17867fd8265bbf90c34040cb1a
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-06-26 21:08:05 +0800

    更

--- scala/The-Lingo/src/test/scala/the/lingo/PropTests.scala
+++ scala/The-Lingo/src/test/scala/the/lingo/PropTests.scala
@@ -11,8 +11,13 @@ import org.scalacheck.Prop.forAll
 import Value.Implicits._
 
 final object PropTests extends Properties("the lingo") {
-  property("parser") = forAll(Gen.identifier, Gen.identifier) { (idx, idy) =>
-    LangParser("file").parseValue(s" (    $idx   $idy  )")
+  private val spaces = Gen.nonEmptyListOf(Gen.oneOf(" ", "\t")).map(_.mkString(""))
+  property("parse 2 elements list") = forAll(spaces, spaces, spaces, Gen.identifier, Gen.identifier) { (s1, s2, s3, idx, idy) =>
+    LangParser("file").parseValue(s"$s1($idx$s2$idy)$s3")
       .equal_reduce_rec(ValueList(List(Sym(idx), Sym(idy))))
+  }
+  property("parse identifier list") = forAll(spaces, spaces, Gen.listOf(Gen.identifier)) { (s, s1, xs) =>
+    LangParser("file").parseValue(s"($s1${xs.mkString(s)})")
+      .equal_reduce_rec(ValueList(xs.map(Sym(_))))
   }
 }
