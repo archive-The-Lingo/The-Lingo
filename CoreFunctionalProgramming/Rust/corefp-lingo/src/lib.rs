9acaf5d5687263f9c463e0f57aa976ba465fe36f
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-05-23 07:20:01 +0000

    up

--- CoreFunctionalProgramming/Rust/corefp-lingo/src/lib.rs
+++ CoreFunctionalProgramming/Rust/corefp-lingo/src/lib.rs
@@ -1,3 +1,23 @@
+use arc_swap::ArcSwap;
+use std::sync::Arc;
+use downcast_rs::Downcast;
+use std::fmt::Debug;
+use trilean::SKleene;
+
+pub trait Values: Downcast + Debug {
+    fn deoptimize(&self) -> CoreValue;
+    fn internal_equal(&self, other: &Value) -> SKleene;
+}
+
+#[derive(Trace, Debug, Clone)]
+pub struct Value(Arc<ArcSwap<dyn Values>>);
+
+pub enum CoreValue {
+    EmptyList,
+    NonEmptyList(Value, Value)
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
