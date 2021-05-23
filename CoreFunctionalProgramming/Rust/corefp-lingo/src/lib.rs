d0c9ce9d16cad9b88093be251f533e173c92fe1c
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-05-23 10:21:45 +0000

    up

--- CoreFunctionalProgramming/Rust/corefp-lingo/src/lib.rs
+++ CoreFunctionalProgramming/Rust/corefp-lingo/src/lib.rs
@@ -6,15 +6,26 @@ use arc_swap::ArcSwap;
 use downcast_rs::Downcast;
 use downcast_rs::impl_downcast;
 use trilean::SKleene;
+use std::ptr;
 
 pub trait Values: Downcast + Debug {
     fn deoptimize(&self) -> CoreValue;
-    fn internal_equal(&self, other: &Value) -> SKleene;
+    fn internal_equal(&self, _other: &Value) -> SKleene {
+        SKleene::Unknown
+    }
+    fn equal(&self, other: &Value) -> bool {
+        match self.internal_equal(other) {
+            SKleene::False => false,
+            SKleene::True => true,
+            SKleene::Unknown => self.deoptimize().core_equal(&other.deoptimize()),
+        }
+    }
 }
 impl_downcast!(Values);
 
 #[derive(Debug, Clone)]
 pub struct Value(Arc<dyn Values>);
+
 impl Deref for Value {
     type Target = Arc<dyn Values>;
 
@@ -23,8 +34,16 @@ impl Deref for Value {
     }
 }
 
+impl Value {
+    pub fn equal(&self, other: &Value) -> bool {
+        if ptr::eq::<dyn Values>(&***self, &***other) { return true; }
+        self.0.equal(other)
+    }
+}
+
 #[derive(Debug)]
 pub struct OptimizableValue(ArcSwap<Value>);
+
 impl Deref for OptimizableValue {
     type Target = ArcSwap<Value>;
 
@@ -32,6 +51,7 @@ impl Deref for OptimizableValue {
         &self.0
     }
 }
+
 impl Values for OptimizableValue {
     fn deoptimize(&self) -> CoreValue {
         self.load().deoptimize()
@@ -50,6 +70,7 @@ pub enum CoreValue {
     Tagged(Value, Value),
     Exception(Value, Value),
 }
+
 impl Values for CoreValue {
     fn deoptimize(&self) -> CoreValue {
         self.clone()
@@ -63,6 +84,18 @@ impl Values for CoreValue {
     }
 }
 
+impl CoreValue {
+    pub fn core_equal(&self, other: &CoreValue) -> bool {
+        match (self, other) {
+            (CoreValue::EmptyList, CoreValue::EmptyList) => true,
+            (CoreValue::Symbol(x), CoreValue::Symbol(y)) => x == y,
+            (CoreValue::NonEmptyList(x0,y0),CoreValue::NonEmptyList(x1,y1)) => x0.equal(x1) && y0.equal(y1),
+            (CoreValue::Tagged(x0,y0),CoreValue::Tagged(x1,y1)) => x0.equal(x1) && y0.equal(y1),
+            (CoreValue::Exception(x0,y0),CoreValue::Exception(x1,y1)) => x0.equal(x1) && y0.equal(y1),
+            (_, _) => false,
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
