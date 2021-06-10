ceff30d3001a737d507d40f045a9d612245a830e
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-06-10 14:41:20 +0800

    up

--- Rel/CoreRel/Scala/corerel-lingo/src/main/scala/lingo/rel/Value.scala
+++ Rel/CoreRel/Scala/corerel-lingo/src/main/scala/lingo/rel/Value.scala
@@ -21,12 +21,6 @@ final case object Value {
     implicit def lift1(x: Value): ValueWithHole = x
 
     implicit def lift2(x: ValueWithHole): Expression = x
-
-    implicit def Tagged_lift0(x: Tagged0): Tagged1 = Tagged1(x.tag, x.list)
-
-    implicit def Tagged_lift1(x: Tagged1): Tagged2 = Tagged2(x.tag, x.list)
-
-    implicit def Tagged_lift2(x: Tagged2): Tagged3 = Tagged3(x.tag, x.list)
   }
 }
 
@@ -34,29 +28,91 @@ import Value.Implicits._
 
 final case object EmptyList extends CoreValue
 
-final case class NonEmptyList(x: CoreValue, xs: CoreValue) extends CoreValue
+// NonEmptyList - begin
+sealed class NonEmptyList3(val tag: Expression, val list: Expression) extends Expression
+
+sealed class NonEmptyList2(override val tag: ValueWithHole, override val list: ValueWithHole) extends NonEmptyList3(tag, list) with ValueWithHole
+
+sealed class NonEmptyList1(override val tag: Value, override val list: Value) extends NonEmptyList2(tag, list) with Value
+
+sealed class NonEmptyList0(override val tag: CoreValue, override val list: CoreValue) extends NonEmptyList1(tag, list) with CoreValue
+
+final object NonEmptyList {
+  def apply(tag: CoreValue, list: CoreValue): NonEmptyList0 = NonEmptyList0(tag, list)
+
+  def apply(tag: Value, list: Value): NonEmptyList1 = NonEmptyList1(tag, list)
+
+  def apply(tag: ValueWithHole, list: ValueWithHole): NonEmptyList2 = NonEmptyList2(tag, list)
+
+  def apply(tag: Expression, list: Expression): NonEmptyList3 = NonEmptyList3(tag, list)
+
+  def unapply(x: NonEmptyList3): Some[(Expression, Expression)] = Some((x.tag, x.list))
+
+  def unapply(x: NonEmptyList2): Some[(ValueWithHole, ValueWithHole)] = Some((x.tag, x.list))
+
+  def unapply(x: NonEmptyList1): Some[(Value, Value)] = Some((x.tag, x.list))
+
+  def unapply(x: NonEmptyList0): Some[(CoreValue, CoreValue)] = Some((x.tag, x.list))
+}
+// NonEmptyList - end
 
 final case class Sym(x: Symbol) extends CoreValue
 
-final case class Tagged0(tag: CoreValue, list: CoreValue) extends CoreValue
+// Tagged - begin
+sealed class Tagged3(val tag: Expression, val list: Expression) extends Expression
 
-final case class Tagged1(tag: Value, list: Value) extends Value
+sealed class Tagged2(override val tag: ValueWithHole, override val list: ValueWithHole) extends Tagged3(tag, list) with ValueWithHole
 
-final case class Tagged2(tag: ValueWithHole, list: ValueWithHole) extends ValueWithHole
+sealed class Tagged1(override val tag: Value, override val list: Value) extends Tagged2(tag, list) with Value
 
-final case class Tagged3(tag: Expression, list: Expression) extends Expression
+sealed class Tagged0(override val tag: CoreValue, override val list: CoreValue) extends Tagged1(tag, list) with CoreValue
 
-final case object Tagged {
-  def apply(tag: CoreValue, list: CoreValue): CoreValue = Tagged0(tag, list)
+final object Tagged {
+  def apply(tag: CoreValue, list: CoreValue): Tagged0 = Tagged0(tag, list)
 
-  def apply(tag: Value, list: Value): Value = Tagged1(tag, list)
+  def apply(tag: Value, list: Value): Tagged1 = Tagged1(tag, list)
 
-  def apply(tag: ValueWithHole, list: ValueWithHole): ValueWithHole = Tagged2(tag, list)
+  def apply(tag: ValueWithHole, list: ValueWithHole): Tagged2 = Tagged2(tag, list)
 
-  def apply(tag: Expression, list: Expression): Expression = Tagged3(tag, list)
+  def apply(tag: Expression, list: Expression): Tagged3 = Tagged3(tag, list)
+
+  def unapply(x: Tagged3): Some[(Expression, Expression)] = Some((x.tag, x.list))
+
+  def unapply(x: Tagged2): Some[(ValueWithHole, ValueWithHole)] = Some((x.tag, x.list))
+
+  def unapply(x: Tagged1): Some[(Value, Value)] = Some((x.tag, x.list))
+
+  def unapply(x: Tagged0): Some[(CoreValue, CoreValue)] = Some((x.tag, x.list))
 }
+// Tagged - end
 
-final case class Exception(tag: CoreValue, list: CoreValue) extends CoreValue
+// Exception - begin
+sealed class Exception3(val tag: Expression, val list: Expression) extends Expression
+
+sealed class Exception2(override val tag: ValueWithHole, override val list: ValueWithHole) extends Exception3(tag, list) with ValueWithHole
+
+sealed class Exception1(override val tag: Value, override val list: Value) extends Exception2(tag, list) with Value
+
+sealed class Exception0(override val tag: CoreValue, override val list: CoreValue) extends Exception1(tag, list) with CoreValue
+
+final object Exception {
+  def apply(tag: CoreValue, list: CoreValue): Exception0 = Exception0(tag, list)
+
+  def apply(tag: Value, list: Value): Exception1 = Exception1(tag, list)
+
+  def apply(tag: ValueWithHole, list: ValueWithHole): Exception2 = Exception2(tag, list)
+
+  def apply(tag: Expression, list: Expression): Exception3 = Exception3(tag, list)
+
+  def unapply(x: Exception3): Some[(Expression, Expression)] = Some((x.tag, x.list))
+
+  def unapply(x: Exception2): Some[(ValueWithHole, ValueWithHole)] = Some((x.tag, x.list))
+
+  def unapply(x: Exception1): Some[(Value, Value)] = Some((x.tag, x.list))
+
+  def unapply(x: Exception0): Some[(CoreValue, CoreValue)] = Some((x.tag, x.list))
+}
+// Exception - end
 
 final case class NonFirstClassRelation() extends CoreValue //TODO
 
