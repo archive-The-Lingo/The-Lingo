ac79fd61b3b900fb726616815f6efb0b5ffb1466
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-09 08:25:15 +0000

    IntelliJ IDEA  is broken!

---
+++ FP/CoreFP/Scala/shared/src/main/scala/lingo/corefp/Value.scala
@@ -0,0 +1,12 @@
+package lingo.corefp
+
+import scala.reflect.runtime.universe.TypeTag
+
+sealed trait Value {
+
+}
+
+object Value {
+  //private val components: java.util.concurrent.ConcurrentHashMap[java.lang.Class, java.util.Map[Value, java.lang.Object]] = new java.util.concurrent.ConcurrentHashMap()
+
+}
