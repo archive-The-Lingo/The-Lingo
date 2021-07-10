979434b5926cb684d4bf8e95ea66ec0199ef5cef
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-10 13:57:26 +0000

    up

--- FP/CoreFP/Scala/shared/src/main/scala/lingo/corefp/Exp.scala
+++ FP/CoreFP/Scala/shared/src/main/scala/lingo/corefp/Exp.scala
@@ -1,5 +1,24 @@
 package lingo.corefp
 
-trait Exp {
+sealed trait Exp {
+  def internal_toValue: Value
+}
 
+object ValueExp extends CachedValueT[Exp] {
+  override def internal_apply(x: Exp): Value = x.internal_toValue
+
+  override def internal_unapply(x: Value): Option[Exp] = x match {
+    case ValueQuote(v) => Some(v)
+    case _ => None
+  }
+}
+
+final case class Quote(x: Value) extends Exp {
+  override def internal_toValue: Value = todo()
+}
+
+object ValueQuote extends CachedValueT[Quote] {
+  override def internal_apply(x: Quote): Value = x.internal_toValue
+
+  override def internal_unapply(x: Value): Option[Quote] = todo()
 }
