dbb570bc04c4ea1e672fac59ed1f30a9cb5a4a94
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-11 08:32:45 +0000

    up

--- FP/CoreFP/Scala/shared/src/main/scala/lingo/corefp/SimpleParser.scala
+++ FP/CoreFP/Scala/shared/src/main/scala/lingo/corefp/SimpleParser.scala
@@ -7,7 +7,9 @@ final case class SimpleParser(file: String = "") {
 
   private def skipWhitepace[_: P] = P(CharsWhileIn(" \r\n\t", 0))
 
-  def value[_: P]: P[Value] = P(atom | list)
+  def value[_: P]: P[Value] = P(atom | list | tagged)
+
+  def exp[_: P]: P[Exp] = P(apply)
 
   // [\u4e00-\u9fa5] is Chinese chars
   private val atomRegex = "(\\w|[-？?/*:><]|[\u4e00-\u9fa5])+".r
@@ -16,5 +18,13 @@ final case class SimpleParser(file: String = "") {
 
   def atom[_: P] = P(CharsWhile(isAtomChar).!).map(Atom(_))
 
-  def list[_: P]: P[Value] = P("(" ~/ skipWhitepace ~/ (value ~ skipWhitepace).rep(sep = whitespace./) ~ skipWhitepace ~ ")").map(xs => ValueList(xs.toList))
+  private def values[_: P]: P[List[Value]] = P(skipWhitepace ~ value.rep(sep = whitespace./)).map(xs => xs.toList)
+
+  private def exps[_: P]: P[List[Exp]] = P(skipWhitepace ~ exp.rep(sep = whitespace./)).map(xs => xs.toList)
+
+  def list[_: P]: P[Value] = P("(" ~/ skipWhitepace ~ values ~ skipWhitepace ~ ")").map(ValueList(_))
+
+  def tagged[_: P]: P[Tagged] = P("#(" ~/ skipWhitepace ~ value ~ whitespace ~ values ~ skipWhitepace ~ ")").map(x => Tagged(x._1, ValueList(x._2)))
+
+  def apply[_: P]: P[Exp] = P("[" ~/ skipWhitepace ~ exp ~ whitespace ~ exps ~ skipWhitepace ~ "]").map(x => ApplyFunction(x._1, x._2))
 }
