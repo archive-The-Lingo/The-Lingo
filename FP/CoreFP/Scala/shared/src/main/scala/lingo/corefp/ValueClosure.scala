cb1d2c70ca6017f2b18bba82a998596af87422d8
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-14 08:33:24 +0000

    up

--- FP/CoreFP/Scala/shared/src/main/scala/lingo/corefp/ValueClosure.scala
+++ FP/CoreFP/Scala/shared/src/main/scala/lingo/corefp/ValueClosure.scala
@@ -1,5 +1,7 @@
 package lingo.corefp
 
+import lingo.corefp.ValueArgs.valueListDotVar
+
 final case class Closure(env: ValueHashMap.Type, args: Args, body: Exp)
 
 object ValueClosure extends CachedValueT[Closure] {
@@ -16,10 +18,18 @@ object ValueClosure extends CachedValueT[Closure] {
 final case class Args(arg: List[Var], rest: Option[Var])
 
 object ValueArgs extends CachedValueT[Args] {
-  private val valuListDot = ValueListDotTU(ValueVar, ValueVar)
+  private val valueListDotVar = ValueListDotTU(ValueVar, ValueVar)
+  private val valueListVar = ValueListT(ValueVar)
   override val helper: ValueArgs.Helper = Helper()
 
-  override def internal_apply(x: Args): Value = todo()
+  override def internal_apply(x: Args): Value = x match {
+    case Args(args, None) => valueListVar(args)
+    case Args(args, Some(rest)) => valueListDotVar(ListDot(args, rest))
+  }
 
-  override def internal_unapply(x: Value): Option[Args] = todo()
+  override def internal_unapply(x: Value): Option[Args] = x match {
+    case valueListVar(args) => Some(Args(args, None))
+    case valueListDotVar(ListDot(args, rest)) => Some(Args(args, Some(rest)))
+    case _ => None
+  }
 }
