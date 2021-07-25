bb09f57f50be1e1adc503462709ea07f63286555
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-25 13:38:44 +0000

    up

--- AttrDT/Scala/shared/src/main/scala/AttrDT.scala
+++ AttrDT/Scala/shared/src/main/scala/AttrDT.scala
@@ -4,7 +4,7 @@ type Identifier = Symbol
 
 type UniqueIdentifier = Int
 
-type Nat = Int
+type NaturalNumber = Int
 
 object UniqueIdentifier {
   private var count: UniqueIdentifier = 0
@@ -19,6 +19,8 @@ object UniqueIdentifier {
 final case class VarId(id: Identifier, uid: UniqueIdentifier)
 
 sealed trait Value
+
+sealed trait Exp
 
 sealed trait BaseType
 
@@ -35,7 +37,7 @@ object AttributeSize {
 
   def apply(x: Option[Value]): AttributeSize = new AttributeSize(x)
 
-  def apply(x: Nat): AttributeSize = new AttributeSize(Some(natToValue(x)))
+  def apply(x: NaturalNumber): AttributeSize = new AttributeSize(Some(natToValue(x)))
 }
 
 sealed trait AttributeUsage
@@ -60,24 +62,44 @@ sealed trait Neu
 
 final case class NeuVar(x: VarId) extends Neu
 
-final case class Zero extends Value
+final case class ZeroV extends Value
 
-final case class Succ(x: Value) extends Value
+final case class Zero extends Exp
 
-final case class NatNum extends BaseType
+final case class SuccV(x: Value) extends Value
 
-def natToValue(x: Nat): Value = if (x == 0) Zero else Succ(natToValue(x - 1))
+final case class Succ(x: Exp) extends Exp
 
-final case class Atom(x: Symbol) extends Value
+final case class NatV extends BaseType
 
-final case class Unit extends Value
+final case class Nat extends Exp
 
-final case class Trivial extends BaseType
+def natToValue(x: NaturalNumber): Value = if (x == 0) ZeroV else SuccV(natToValue(x - 1))
 
-final case class Cons(a: Value, d: Value) extends Value
+final case class Atom(x: String) extends Value
 
-final case class Sigma(a: TypeOrNotYet, d: Closure) extends BaseType
+final case class Quote(x: String) extends Exp
+
+final case class UnitV extends Value
+
+final case class Unit extends Exp
+
+final case class TrivialV extends BaseType
+
+final case class Trivial extends Exp
+
+final case class ConsV(a: Value, d: Value) extends Value
+
+final case class Cons(a: Exp, d: Exp) extends Exp
+
+final case class SigmaV(a: TypeOrNotYet, d: Closure) extends BaseType
+
+final case class Sigma(a: Exp, aId: Identifier, d: Exp) extends Exp
 
 final case class Closure(id: NeuVar, body: Value) extends Value
 
-final case class Pi(a: TypeOrNotYet, d: Closure) extends BaseType
+final case class Lambda(id: Identifier, body: Exp) extends Exp
+
+final case class PiV(domain: TypeOrNotYet, codomain: Closure) extends BaseType
+
+final case class Pi(domain: Exp, domainId: Identifier, codomain: Exp) extends Exp
